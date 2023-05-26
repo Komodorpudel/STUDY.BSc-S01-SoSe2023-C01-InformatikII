925ee72e47557277f7c1ef83ee8fcf4e743ee6fc
Komodorpudel <sah_services@outlook.com>
2023-05-26 17:32:55 +0000

    asd

--- 01 - Übungsblätter/05 - WIP/Aufgabe_20/Aufgabe_20a.java
+++ 01 - Übungsblätter/05 - WIP/Aufgabe_20/Aufgabe_20a.java
@@ -8,13 +8,22 @@ public class Aufgabe_20a {
     // --------------------------------------
 
     public static void main(String[] args) {
+
+        // Random Seed
         Random random = new Random();
+
+        // 1. unter Benutzung der ints-Methode der Klasse Random ein IntStream-Objekt mit
+        //    1000 Zufallszahlen zwischen 0 (einschließlich) und 100 (ausschließlich) erzeugt,
         int sum = random.ints(0, 100)
                 .limit(1000)
+                // 2. alle enthaltenen Werte ganzzahlig durch 10 teilt,
                 .map(n -> n / 10)
+                // 3. alle Duplikate mit einer geeigneten Methode aus dem Stream entfernt,
                 .distinct()
-                .sum();
+                // 4. die Summe aller verbleibenden Zahlen berechnet ...
+                .sum(); 
 
+        // ... und ausgibt. (sum ist quasi immer 45 (1+2+3+4+5+6+7+8+9))
         System.out.println("Summe: " + sum);
     }
 
--- 01 - Übungsblätter/05 - WIP/Aufgabe_20/Aufgabe_20c.java
+++ 01 - Übungsblätter/05 - WIP/Aufgabe_20/Aufgabe_20c.java
@@ -5,31 +5,43 @@ import java.util.stream.IntStream;
 import java.util.stream.Stream;
 //
 
-public class Main {
+public class Aufgabe_20c {
 
     // --------------------------------------
 
     public static void main(String[] args) {
+
+        // Random seed
         Random random = new Random();
-        Supplier<Character> charSupplier = () -> (char) ('a' + random.nextInt(26));
-        Supplier<String> stringSupplier = () -> {
-            int length = 5 + random.nextInt(6);
-            StringBuilder sb = new StringBuilder();
-            for (int i = 0; i < length; i++) {
-                sb.append(charSupplier.get());
+
+        // 1. ein Supplier-Objekt fur ¨ Character-Objekte mit zuf¨alligen, lateinischen Kleinbuchstaben erzeugt,
+        Supplier<Character> characterSupplier = () -> (char) (random.nextInt(26) + 'a');
+
+        // 2. ein Stream-Objekt fur ¨ String-Objekte erzeugt, welches das zuvor erstellte SupplierObjekt verwendet,
+        //    um W¨orter bestehend aus zuf¨alligen lateinischen Kleinbuchstaben ...
+        Stream<String> wordStream = Stream.generate(() -> {
+            StringBuilder wordBuilder = new StringBuilder();
+
+            // ... mit zuf¨alliger L¨ange zwischen 5 und 10 Zeichen (jeweils einschließlich) zu erzeugen,
+            int length = 5 + random.nextInt(6); 
+            for (int i = 0; i < length; i = i + 1) {
+                wordBuilder.append(characterSupplier.get());
             }
-            return sb.toString();
-        };
+            return wordBuilder.toString();
+            
+        })
+        // 3. die L¨ange dieses Streams auf 500 beschr¨ankt,
+        .limit(500); // Begrenzen Sie den Stream auf 500 Wörter
 
-        double averageLength = Stream.generate(stringSupplier)
-                .limit(500)
-                .mapToInt(String::length)
-                .average()
-                .orElseThrow();
+        // 4. einen IntStream mit den L¨angen der Worte erzeugt und
+        IntStream lengthStream = wordStream.mapToInt(String::length);
 
-        System.out.println("Durchschnittliche Länge: " + averageLength);
+        // 5. den Durchschnittswert dieser L¨angen berechnet und ausgibt (verwenden Sie die
+        //    orElseThrow()-Methode des erhaltenen OptionalDouble-Objects).
+        double average = lengthStream.average().orElseThrow();
+        System.out.println("Durchschnittliche Wortlänge: " + average);
     }
 
     // --------------------------------------
-
+    
 }
