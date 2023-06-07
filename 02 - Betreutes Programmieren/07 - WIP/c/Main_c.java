63b5889c40474bc4ed5cb0d2faece5e25ba0e429
Komodorpudel <sah_services@outlook.com>
2023-06-07 16:07:20 +0200

    sad

--- 02 - Betreutes Programmieren/07 - WIP/c/Main_c.java
+++ 02 - Betreutes Programmieren/07 - WIP/c/Main_c.java
@@ -1,5 +1,11 @@
 public class Main_c {
+
+    // --------------------------------------
+
     public static void main(String[] args) {
         new TextConverterGUI_c();
     }
+
+    // --------------------------------------
+
 }
