64e574e94d793e7dbe4853ad7f42dda44524008d
komodorpudel <sah_services@outlook.com>
2023-06-11 19:11:50 +0200

    sad

--- 01 - Übungsblätter/06 - WIP/A29/A29a_main.java
+++ 01 - Übungsblätter/06 - WIP/A29/A29a_main.java
@@ -1,35 +1,97 @@
-import javax.swing.*;
-import java.awt.*;
+
+// Aufgabenstellung:
+    // 1. Es soll den Titel Aufgabe29a haben.
+
+    // 2. Es soll im Norden von links nach rechts angeordnet die Beschriftung Kontonummer und ein aktiviertes einzeiliges Textfeld der Breite 20 anzeigen.
+    // 2.a Beschriftung und Textfeld sollen beide dieselbe Schrift mit Schriftgr¨oße 20 haben.
+
+    // 3. Es soll im Suden von links nach rechts angeordnet zwei Schaltfl ¨ ¨achen mit dem Text Okay und Abbrechen anzeigen.
+    // 4. Der Nord- und der Sudbereich sollen unterschiedliche Graufarben als Hintergrundfarbe ¨haben.
+    // 5. Durch Klicken auf das Fensterkreuz soll die Anwendung beendet werden.
+//
+
+// Necessary packages/classes:
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.BorderLayout;
+//
 
 public class A29a_main extends JFrame {
 
+
+    // --------------------------------------
+
+    // Attribute:
+    private JPanel northPanel;
+    private JLabel label;
+    private JTextField textField;
+
+    private JPanel southPanel;
+    private JButton okButton;
+    private JButton cancelButton;
+
+    // --------------------------------------
+
+    // Konstruktor:
     public A29a_main() {
-        
+
+        // ++++++++++++++++++++++++++
+
         // Fenstertitel
+        // 1. Es soll den Titel Aufgabe29a haben.
         setTitle("Aufgabe29a");
 
+        // ++++++++++++++++++++++++++
+
         // Nordbereich
-        JPanel northPanel = new JPanel();
+        northPanel = new JPanel();
+
+        // 4. Der Nord- und der Sudbereich sollen unterschiedliche Graufarben als Hintergrundfarbe ¨haben.
         northPanel.setBackground(Color.LIGHT_GRAY);
-        JLabel label = new JLabel("Kontonummer");
-        JTextField textField = new JTextField(20);
+
+        label = new JLabel("Kontonummer");
+        textField = new JTextField(20);
+
+
+        // 2.a Beschriftung und Textfeld sollen beide dieselbe Schrift mit Schriftgr¨oße 20 haben.
         label.setFont(new Font("Arial", Font.PLAIN, 20));
         textField.setFont(new Font("Arial", Font.PLAIN, 20));
+
+        // 2. Es soll im Norden von links nach rechts angeordnet die Beschriftung Kontonummer und ein aktiviertes einzeiliges Textfeld der Breite 20 anzeigen.
         northPanel.add(label);
         northPanel.add(textField);
+
         add(northPanel, BorderLayout.NORTH);
 
+        // ++++++++++++++++++++++++++
+
         // Südbereich
-        JPanel southPanel = new JPanel();
+        southPanel = new JPanel();
+
+        // 4. Der Nord- und der Sudbereich sollen unterschiedliche Graufarben als Hintergrundfarbe ¨haben.
         southPanel.setBackground(Color.DARK_GRAY);
-        JButton okButton = new JButton("Okay");
-        JButton cancelButton = new JButton("Abbrechen");
+
+        okButton = new JButton("Okay");
+        cancelButton = new JButton("Abbrechen");
+
+        // 3. Es soll im Suden von links nach rechts angeordnet zwei Schaltfl ¨ ¨achen mit dem Text Okay und Abbrechen anzeigen.
         southPanel.add(okButton);
         southPanel.add(cancelButton);
+
         add(southPanel, BorderLayout.SOUTH);
 
+        // ++++++++++++++++++++++++++
+
         // Fenster schließen
+        // 5. Durch Klicken auf das Fensterkreuz soll die Anwendung beendet werden.
         setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        // ++++++++++++++++++++++++++
 
         // Fenstergröße und Sichtbarkeit
         setSize(500, 200);
@@ -37,7 +99,13 @@ public class A29a_main extends JFrame {
         setVisible(true);
     }
 
+    // --------------------------------------
+
+    // main
     public static void main(String[] args) {
         new A29a_main();
     }
+
+    // --------------------------------------
+    
 }
--- 01 - Übungsblätter/06 - WIP/A29/A29b_main.java
+++ 01 - Übungsblätter/06 - WIP/A29/A29b_main.java
@@ -1,51 +1,105 @@
-import javax.swing.*;
-import java.awt.*;
+
+// Necessary packages/classes:
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.GridLayout;
+import java.awt.Font;
+import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+//
 
-public class A29b_main extends JFrame implements ActionListener {
+public class A29b_main extends JFrame {
 
-    private JTextField displayField;
+    // --------------------------------------
 
+    // Attribute:
+    private JPanel inputPanel;
+    private JTextField inputField;
+
+    private JPanel buttonPanel;
+    private JButton[] numberButtons;
+    private JButton clearButton;
+    private JButton okayButton;
+
+    // --------------------------------------
+
+    // Konstruktor:
     public A29b_main() {
+
+        // ++++++++++++++++++++++++++
+
         // Fenstertitel
-        setTitle("Aufgabe29b");
+        setTitle("A29b");
 
-        // Oberer Bereich zur Anzeige der Eingabe
-        JPanel topPanel = new JPanel();
-        topPanel.setBackground(Color.WHITE);
-        topPanel.setPreferredSize(new Dimension(300, 100));
-        displayField = new JTextField(20);
-        displayField.setFont(new Font("Arial", Font.PLAIN, 20));
-        displayField.setEditable(false);
-        topPanel.add(displayField);
-        add(topPanel, BorderLayout.NORTH);
+        // ++++++++++++++++++++++++++
 
-        // Unterer Bereich mit Zahlen und Schaltflächen
-        JPanel buttonPanel = new JPanel(new GridLayout(4, 3));
-        buttonPanel.setBackground(Color.LIGHT_GRAY);
+        // A. Weißes Feld für Input
+        inputPanel = new JPanel();
+        inputPanel.setLayout(new BorderLayout());
 
-        // Tasten hinzufügen
-        for (int i = 1; i <= 9; i++) {
-            JButton button = new JButton(String.valueOf(i));
-            button.addActionListener(this);
-            buttonPanel.add(button);
+        
+        inputField = new JTextField();
+        inputField.setFont(new Font("Arial", Font.PLAIN, 20));
+        inputField.setEditable(false);
+        inputPanel.add(inputField, BorderLayout.CENTER);
+
+        add(inputPanel, BorderLayout.NORTH);
+
+        // ++++++++++++++++++++++++++
+
+        // B. Buttons
+        buttonPanel = new JPanel();
+        buttonPanel.setLayout(new GridLayout(4, 3));
+
+        numberButtons = new JButton[10]; // 0 to 9
+
+        // Add buttons 7, 8, 9
+        for (int i = 7; i <= 9; i = i + 1) {
+            numberButtons[i-1] = createNumberButton(String.valueOf(i));
+            buttonPanel.add(numberButtons[i-1]);
         }
-        JButton clearButton = new JButton("Clear");
-        clearButton.addActionListener(this);
-        JButton zeroButton = new JButton("0");
-        zeroButton.addActionListener(this);
-        JButton okayButton = new JButton("Okay");
-        okayButton.addActionListener(this);
 
+        // Add buttons 4, 5, 6
+        for (int i = 4; i <= 6; i = i + 1) {
+            numberButtons[i-1] = createNumberButton(String.valueOf(i));
+            buttonPanel.add(numberButtons[i-1]);
+        }
+
+        // Add buttons 1, 2, 3
+        for (int i = 1; i <= 3; i = i + 1) {
+            numberButtons[i-1] = createNumberButton(String.valueOf(i));
+            buttonPanel.add(numberButtons[i-1]);
+        }
+
+        // Add Clear, 0, Okay buttons
+        clearButton = new JButton("Clear");
+        clearButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                inputField.setText("");
+            }
+        });
         buttonPanel.add(clearButton);
-        buttonPanel.add(zeroButton);
+
+        numberButtons[0] = createNumberButton("0");
+        buttonPanel.add(numberButtons[0]);
+
+        okayButton = new JButton("Okay");
+        
         buttonPanel.add(okayButton);
 
         add(buttonPanel, BorderLayout.CENTER);
 
+        add(buttonPanel, BorderLayout.CENTER);
+
+        // ++++++++++++++++++++++++++
+
         // Fenster schließen
         setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        // ++++++++++++++++++++++++++
 
         // Fenstergröße und Sichtbarkeit
         pack();
@@ -53,19 +107,26 @@ public class A29b_main extends JFrame implements ActionListener {
         setVisible(true);
     }
 
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        String command = e.getActionCommand();
-        if ("Clear".equals(command)) {
-            displayField.setText("");
-        } else if ("Okay".equals(command)) {
-            // Hier können Sie die gewünschte Aktion für die Schaltfläche "Okay" definieren.
-        } else {
-            displayField.setText(displayField.getText() + command);
-        }
+    // --------------------------------------
+
+    private JButton createNumberButton(String text) {
+
+        JButton button = new JButton(text);
+        
+        button.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                inputField.setText(inputField.getText() + text);
+            }
+        });
+        return button;
     }
+
+    // --------------------------------------
 
     public static void main(String[] args) {
         new A29b_main();
     }
+
+    // --------------------------------------
+
 }
