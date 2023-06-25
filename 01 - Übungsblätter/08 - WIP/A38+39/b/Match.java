20f5a764f2357e62e9baeb4c7f4eb6e1a0595298
Komodorpudel <sah_services@outlook.com>
2023-06-26 01:43:41 +0200

    sad

---
+++ 01 - Übungsblätter/08 - WIP/A38+39/b/Match.java
@@ -0,0 +1,19 @@
+
+public class Match {
+
+    // --------------------------------------
+
+    // Attribute
+    private Team team1;
+    private Team team2;
+    private Team winner;
+
+    // --------------------------------------
+
+    // {winner == team1 or winner = team2}
+    // {team2 != team1}
+    // Implement via Setter
+
+    // --------------------------------------
+    
+}
